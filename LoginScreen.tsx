cad37c8747853583e9cb4ced3a79281a167beed8
turanbuyukkamaci <nazimhikmet566@gmail.com>
2024-08-11 19:03:18 +0300

    Last changes were made on 20240811

--- LoginScreen.tsx
+++ LoginScreen.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import Modal from 'react-native-modal';
 
-const LoginScreen: React.FC<{navigation: any}> = ({navigation}) => {
+const LoginScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.background}>
       <Image
@@ -16,13 +19,34 @@ const LoginScreen: React.FC<{navigation: any}> = ({navigation}) => {
         </Text>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => {
-            console.log('Button Pressed. Attempting to navigate.');
-            navigation.navigate('Registration');
-          }}>
+          onPress={() => setModalVisible(true)}>
           <Text style={styles.buttonText}>Hemen Başlayın!</Text>
         </TouchableOpacity>
       </View>
+      <Modal
+        isVisible={modalVisible}
+        onSwipeComplete={() => setModalVisible(false)}
+        swipeDirection="down"
+        style={styles.modal}>
+        <View style={styles.modalContent}>
+          <TouchableOpacity
+            style={styles.modalButton}
+            onPress={() => {
+              setModalVisible(false);
+              navigation.navigate('SomeScreenForGoogleLogin');
+            }}>
+            <Text style={styles.modalText}>Google ile Giriş Yapınız</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.modalButton}
+            onPress={() => {
+              setModalVisible(false);
+              navigation.navigate('HomeScreen');
+            }}>
+            <Text style={styles.modalText}>Üyeliksiz Devam Et</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -32,13 +56,13 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
     height: '100%',
-    backgroundColor: '#fcf4e4', // Light cream background color
+    backgroundColor: '#fcf4e4',
     justifyContent: 'center',
     alignItems: 'center',
   },
   logo: {
-    width: 300, // Increased width
-    height: 300, // Increased height
+    width: 300,
+    height: 300,
     flex: 1,
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -49,7 +73,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 20,
-    paddingTop: 10, // Adjusted top padding to accommodate larger logo
+    paddingTop: 10,
   },
   welcomeText: {
     color: '#8a4412',
@@ -61,7 +85,7 @@ const styles = StyleSheet.create({
     color: '#8a4412',
     fontSize: 18,
     textAlign: 'center',
-    marginBottom: 100, // Reduced margin to position the button a bit lower
+    marginBottom: 100,
   },
   button: {
     backgroundColor: '#8a4412',
@@ -72,12 +96,41 @@ const styles = StyleSheet.create({
     width: 350,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 100, // Ensures button is towards the bottom
+    marginBottom: 100,
   },
   buttonText: {
     color: '#fcf4e4',
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  modal: {
+    justifyContent: 'flex-end',
+    margin: 0,
+  },
+  modalContent: {
+    backgroundColor: '#fcf4e4',
+    padding: 75,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  modalButton: {
+    backgroundColor: '#8a4412',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    width: 350,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  modalText: {
+    color: '#fcf4e4',
+    textAlign: 'center',
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
 
